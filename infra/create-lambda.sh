f28e14f47d8d8c882b9f8f0b640c7a63850cbc63
siddhant <32503425+Siddhantt@users.noreply.github.com>
2025-07-09 01:39:45 +0530

    Update create-lambda.sh

--- infra/create-lambda.sh
+++ infra/create-lambda.sh
@@ -2,21 +2,17 @@
 
 set -e
 
-FUNCTION_NAME="ContactFormHandler"
-ROLE_ARN=$(aws iam get-role --role-name lambda-dynamodb-role --query 'Role.Arn' --output text)
+FUNCTION_NAME="myLambdaFunction"
+ZIP_FILE="lambda_function.zip"
+ROLE_ARN="arn:aws:iam::326641642949:role/lambda-dynamodb-role"  # Use the ARN of your role
 
 echo "Zipping Lambda function..."
-cd backend
-zip function.zip lambda_function.py  # Make sure to zip the Python function file
-cd ..
+zip $ZIP_FILE lambda_function.py
 
 echo "Deploying Lambda function..."
-
 aws lambda create-function \
   --function-name $FUNCTION_NAME \
-  --runtime python3.8 \  # Change runtime to python3.8 or another supported version
-  --handler lambda_function.lambda_handler \  # This should be your file name and function name
-  --role $ROLE_ARN \
-  --zip-file fileb://backend/function.zip
-
-echo "Lambda function deployed: $FUNCTION_NAME"
+  --zip-file fileb://$ZIP_FILE \
+  --handler lambda_function.lambda_handler \
+  --runtime python3.8 \
+  --role $ROLE_ARN
