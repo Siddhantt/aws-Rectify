4833da9df6f577e8e93745d94e580b01a0b188b6
siddhantt <siddhanttayade98@gmail.com>
2025-07-08 14:08:01 +0000

    Region configuration

--- infra/create-s3.sh
+++ infra/create-s3.sh
@@ -3,11 +3,15 @@
 set -e
 
 BUCKET_NAME="siddhant-portfolio-2025"
+AWS_REGION="ap-south-1"  # Specify your region here
 
-echo "Creating S3 bucket: $BUCKET_NAME..."
+echo "Creating S3 bucket: $BUCKET_NAME in region: $AWS_REGION..."
+
+# Create the S3 bucket with the specified region
 aws s3api create-bucket \
   --bucket $BUCKET_NAME \
-  --region ap-south-1
+  --region $AWS_REGION \
+  --create-bucket-configuration LocationConstraint=$AWS_REGION
 
 echo "Enabling static website hosting..."
 aws s3 website s3://$BUCKET_NAME/ \
@@ -23,4 +27,4 @@ echo "Syncing website content..."
 aws s3 sync ./frontend s3://$BUCKET_NAME/ --delete
 
 echo "S3 website deployed at:"
-echo "http://$BUCKET_NAME.s3-website-ap-south-1.amazonaws.com"
+echo "http://$BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com"
