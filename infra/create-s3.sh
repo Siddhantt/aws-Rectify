2a0ffb52dcff4702ffca6023c351619a9d30ebab
siddhant <32503425+Siddhantt@users.noreply.github.com>
2025-07-08 20:45:48 +0530

    Update create-s3.sh
    Revert

--- infra/create-s3.sh
+++ infra/create-s3.sh
@@ -5,22 +5,12 @@ set -e
 BUCKET_NAME="siddhant-portfolio-2025"
 AWS_REGION="ap-south-1"  # Specify your region here
 
-# Check if the bucket already exists
-echo "Checking if S3 bucket exists: $BUCKET_NAME..."
-BUCKET_EXISTS=$(aws s3api head-bucket --bucket $BUCKET_NAME --region $AWS_REGION 2>&1)
-
-if [[ $BUCKET_EXISTS == *"Not Found"* ]]; then
-  echo "Bucket does not exist. Creating S3 bucket: $BUCKET_NAME in region: $AWS_REGION..."
-  
-  # Create the S3 bucket with the specified region
-  aws s3api create-bucket \
-    --bucket $BUCKET_NAME \
-    --region $AWS_REGION \
-    --create-bucket-configuration LocationConstraint=$AWS_REGION
-
-else
-  echo "Bucket $BUCKET_NAME already exists. Skipping creation..."
-fi
+# Create the S3 bucket with the specified region
+echo "Creating S3 bucket: $BUCKET_NAME in region: $AWS_REGION..."
+aws s3api create-bucket \
+  --bucket $BUCKET_NAME \
+  --region $AWS_REGION \
+  --create-bucket-configuration LocationConstraint=$AWS_REGION
 
 # Enabling static website hosting
 echo "Enabling static website hosting..."
@@ -40,4 +30,3 @@ aws s3 sync ./frontend s3://$BUCKET_NAME/ --delete
 
 echo "S3 website deployed at:"
 echo "http://$BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com"
-
