cfd222088b99c454884044d3348c542a9b00a3b0
siddhant <32503425+Siddhantt@users.noreply.github.com>
2025-07-10 01:25:07 +0530

    Update lambda_function.py

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -44,13 +44,13 @@ def lambda_handler(event, context):
         })
 
         return {
-    'statusCode': 200,
-    'headers': {
+        'statusCode': 200,
+        'headers': {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
         'Access-Control-Allow-Methods': 'OPTIONS,POST'
     },
-    'body': json.dumps({'message': 'Message saved successfully'})
+        'body': json.dumps({'message': 'Message saved successfully'})
 }
 
     except Exception as e:
